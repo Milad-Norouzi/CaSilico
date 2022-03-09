bbcd5286a369c18b6a754822b9914b71bfa473af
Mohammad Reza Bakhtiarizadeh <9910942+mrb20045@users.noreply.github.com>
2022-03-09 23:43:16 +0330

    first commit

---
+++ R/exam.R
@@ -0,0 +1,19 @@
+#' Festa file related to the Foot-and-mouth disease virus 3D gene
+
+#'
+#' @docType data
+#'
+#' @usage data(example_seq)
+#'
+#' @format An object of class `"fasta"`.
+#'
+#' @keywords example
+#'
+#'
+#' @source Foot-and-mouth disease virus, <https://www.ncbi.nlm.nih.gov/>
+#'
+#' @examples
+#' data(example_seq)
+#' seq <- example
+#'
+"example_seq"
